f179982dab61b86fd5a4ed2d66b123c1f7a6cfda
Danilo Andrade <mandrade.danilo@gmail.com>
2018-07-09 16:51:09 -0300

    merge support repository

---
+++ AbstractEnum.php
@@ -0,0 +1,267 @@
+<?php
+/**
+ *  Copyright (c) 2018 Danilo Andrade (http://daniloandrade.net)
+ *
+ *  This file is part of the Aplí Framework.
+ *
+ * @project Aplí Framework
+ * @file Environment.php
+ * @author Danilo Andrade <danilo@daniloandrade.net>
+ * @date 07/07/18 at 17:10
+ * @copyright  Copyright (c) 2018 Danilo Andrade
+ * @license    GNU Lesser General Public License version 3 or later.
+ */
+
+namespace Apli\Support;
+
+use ReflectionException;
+use UnexpectedValueException;
+
+/**
+ * Enum implementation inspired from SplEnum
+ *
+ * @package Apli\Support
+ */
+abstract class AbstractEnum implements \JsonSerializable
+{
+
+    /**
+     * Store existing constants in a static cache per object.
+     *
+     * @var array
+     */
+    protected static $cache = array();
+    /**
+     * Set name of default constant key
+     * @var string
+     */
+    protected static $defaultKey = "__default";
+    /**
+     * Enum value
+     *
+     * @var mixed
+     */
+    protected $value;
+
+    /**
+     * Creates a new value of some type
+     *
+     * @param mixed|null $value Initial value
+     * @param bool $strict Provided for SplEnum compatibility (its purpose is unknown)
+     *
+     * @throws UnexpectedValueException if incompatible type is given.
+     * @throws ReflectionException
+     */
+    public function __construct($value = null, $strict = false)
+    {
+        $className = get_called_class();
+
+        if (is_null($value)) {
+            if (!self::isValidName(self::$defaultKey)) {
+                throw new UnexpectedValueException('Default value not defined in enum ' . $className);
+            }
+
+            $value = self::getConstants()[self::$defaultKey];
+        }
+
+        if (!self::isValidValue($value)) {
+            throw new UnexpectedValueException("Value '$value' is not part of the enum " . get_called_class());
+        }
+
+        $this->value = $value;
+    }
+
+    /**
+     * Check if enum key exists
+     *
+     * @param string $name Name of the constant to validate
+     * @param bool $strict Case is significant when searching for name
+     * @return bool
+     * @throws ReflectionException
+     */
+    public static function isValidName($name, $strict = true)
+    {
+        $constants = static::getConstants();
+
+        if ($strict) {
+            return array_key_exists($name, $constants);
+        }
+
+        $constantNames = array_map('strtoupper', array_keys($constants));
+        return in_array(strtoupper($name), $constantNames);
+    }
+
+    /**
+     * Returns all enum constants
+     *
+     * @param bool $includeDefault
+     * @return array|mixed
+     * @throws ReflectionException
+     */
+    public static function getConstants($includeDefault = true)
+    {
+        $className = get_called_class();
+        if (!array_key_exists($className, static::$cache)) {
+            $reflection = new \ReflectionClass($className);
+            static::$cache[$className] = $reflection->getConstants();
+        }
+
+        $constants = self::$cache[$className];
+
+        if ($includeDefault === false) {
+            $constants = array_filter(
+                $constants,
+                function ($key) {
+                    return $key !== self::$defaultKey;
+                },
+                ARRAY_FILTER_USE_KEY);
+        }
+
+        return $constants;
+    }
+
+    /**
+     * Check if is valid enum value
+     *
+     * @param $value
+     * @return bool
+     * @throws ReflectionException
+     */
+    public static function isValidValue($value)
+    {
+        return in_array($value, static::toArray(), true);
+    }
+
+    /**
+     * Returns all possible values as an array, except default constant
+     *
+     * @return mixed
+     * @throws ReflectionException
+     */
+    public static function toArray()
+    {
+        return self::getConstants(false);
+    }
+
+    /**
+     * Returns the names (keys) of all constants in the Enum class
+     *
+     * @return array
+     * @throws ReflectionException
+     */
+    public static function keys()
+    {
+        return array_keys(static::toArray());
+    }
+
+    /**
+     * Returns instances of the Enum class of all Enum constants
+     *
+     * @return array
+     * @throws ReflectionException
+     */
+    public static function values()
+    {
+        $values = array();
+        foreach (static::toArray() as $key => $value) {
+            $values[$key] = new static($value);
+        }
+        return $values;
+    }
+
+    /**
+     * Provided for compatibility with SplEnum
+     *
+     * @see AbstractEnum::getConstants()
+     * @param bool $include_default Include `__default` and its value. Not included by default.
+     * @return array
+     * @throws ReflectionException
+     */
+    public static function getConstList($include_default = false)
+    {
+        return self::getConstants($include_default);
+    }
+
+    /**
+     * Returns a value when called statically like so: MyEnum::SOME_VALUE() given SOME_VALUE is a class constant
+     *
+     * @param $name
+     * @param $arguments
+     * @return AbstractEnum
+     * @throws ReflectionException
+     * @throws \BadMethodCallException if enum does not exist
+     */
+    public static function __callStatic($name, $arguments)
+    {
+        $array = static::toArray();
+        if (isset($array[$name])) {
+            return new static($array[$name]);
+        }
+        throw new \BadMethodCallException("No static method or enum constant '$name' in class " . get_called_class());
+    }
+
+    /**
+     * Returns the enum key.
+     *
+     * @return mixed
+     * @throws ReflectionException
+     */
+    public function getKey()
+    {
+        return static::search($this->value);
+    }
+
+    /**
+     * Return key for value
+     *
+     * @param $value
+     * @return false|int|string
+     * @throws ReflectionException
+     */
+    public static function search($value)
+    {
+        return array_search($value, static::toArray(), true);
+    }
+
+    /**
+     * Return string representation of the enum's value
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return strval($this->value);
+    }
+
+    /**
+     * Compares one Enum with another.
+     *
+     * @param AbstractEnum|null $enum
+     * @return bool
+     */
+    final public function equals(AbstractEnum $enum = null)
+    {
+        return $enum !== null && $this->getValue() === $enum->getValue() && get_called_class() == get_class($enum);
+    }
+
+    /**
+     * Get enum value
+     *
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Specify data which should be serialized to JSON. This method returns data that can be serialized by json_encode()
+     * natively.
+     *
+     * @return mixed
+     */
+    public function jsonSerialize()
+    {
+        return $this->getValue();
+    }
+}
---
+++ README.md
@@ -0,0 +1,93 @@
+# Apli Support
+       
+Apli Support package provides some core useful tool set.
+
+## Installation via Composer
+
+Add this to the require block in your `composer.json`.
+
+``` json
+{
+    "require": {
+        "apli/support": "~1.0"
+    }
+}
+```
+
+## Enum
+
+Enum support class implementation inspired from SplEnum
+
+### Declaration
+
+```php
+use Apli\Support\AbstractEnum;
+
+/**
+ * State enum
+ */
+class State extends AbstractEnum
+{
+    const ACTIVE = 'active';
+    const INACTIVE = 'inactive';
+    const EXPIRED = 'expired';
+    const DESTROYED = 'destroyed';
+    const ERROR = 'error';
+}
+```
+
+You can declare the enum class using `final` or set the constant to `private` (only supported in PHP>7.1)
+
+### Usage
+
+```php
+$state = new State(State::ACTIVE);
+// or
+$state = State::ACTIVE();
+```
+
+Static methods are automatically implemented to provide quick access to an enum value.
+
+As each constant is a class you have the advantage of type-hint:
+
+```php
+function setState(State $state) {
+    // ...
+}
+```
+
+### Complete list of methods
+
+- `__toString()` You can `echo $state`, it will display the enum value (value of the constant)
+- `getValue()` Returns the current value of the enum
+- `getKey()` Returns the key of the current value on Enum
+- `equals()` Tests whether enum instances are equal to another
+
+Static methods:
+
+- `toArray()` returns all possible values as an array
+- `keys()` Returns the names (keys) of all constants in the Enum class
+- `values()` Returns instances of the Enum class of all Enum constants (constant name in key, Enum instance in value)
+- `isValidValue()` Check if tested value is valid on enum set
+- `isValidName()` Check if tested key is exists on enum set
+- `search()` Return key for searched value
+
+### Static methods
+
+Static method helpers are implemented using [`__callStatic()`](http://www.php.net/manual/en/language.oop5.overloading.php#object.callstatic).
+
+If you care about IDE autocompletion, you can use phpdoc (this is supported in PhpStorm for example):
+
+```php
+/**
+ * Class State
+ *
+ * @method static State ACTIVE()
+ * @method static State INACTIVE()
+ */
+class State extends AbstractEnum
+{
+    const ACTIVE = 'active';
+    const INACTIVE = 'inactive';
+}
+```
