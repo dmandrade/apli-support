d427c5e236f81ee7e631508f070f1428f0c322c7
Danilo Andrade <mandrade.danilo@gmail.com>
2018-09-04 16:47:20 -0300

    add a base class for a future http status code enum

---
+++ HttpStatus.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *  Copyright (c) 2018 Danilo Andrade
+ *
+ *  This file is part of the apli project.
+ *
+ *  @project apli
+ *  @file HttpStatus.php
+ *  @author Danilo Andrade <danilo@webbingbrasil.com.br>
+ *  @date 04/09/18 at 11:57
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: Danilo
+ * Date: 04/09/2018
+ * Time: 11:57
+ */
+
+namespace Apli\Support;
+
+
+class HttpStatus extends AbstractEnum
+{
+
+}
